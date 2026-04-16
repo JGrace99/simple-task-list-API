e85d7f7e14c4f924e18a4996d0e66c6a28f8ca19
Jack Grace <jackgrace@Jacks-MacBook-Air-2.local>
2026-04-16 01:28:28 -0400

    feat: database initialization and Task Data Model

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { createServer } from 'node:http'
-import { createYoga } from 'graphql-yoga'
-import { schema } from './schema/builder.js'
+import { createServer } from "node:http";
+import { createYoga } from "graphql-yoga";
+import { schema } from "./schema/builder.js";
 
 // Schemas are designed using Pothos builder, then export and imported by Yoga.
-// The schema is essentially Yogas rulebook and instruction manual. 
+// The schema is essentially Yogas rulebook and instruction manual.
 // It defines what fields and types exist (validation), and maps each field to its resolver function (execution)
-const yoga = createYoga({ schema })
+const yoga = createYoga({ schema });
 
 // Creates a node.js server using the previously created yoga instance as a listener
-const server = createServer(yoga)
+const server = createServer(yoga);
 
 server.listen(4000, () => {
-    console.log('Server running on http://localhost:4000/graphql')
-})
+  console.log("Server running on http://localhost:4000/graphql");
+});
